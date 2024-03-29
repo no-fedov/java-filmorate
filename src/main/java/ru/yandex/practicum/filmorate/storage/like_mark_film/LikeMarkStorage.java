2535f929ffdba5cb4384c31fabcc20adecb55288
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-03-29 22:54:21 +0300

    tz 11

--- src/main/java/ru/yandex/practicum/filmorate/storage/like_mark_film/LikeMarkStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like_mark_film/LikeMarkStorage.java
@@ -2,5 +2,6 @@ package ru.yandex.practicum.filmorate.storage.like_mark_film;
 
 public interface LikeMarkStorage {
     void addLikeFilm(int filmId, int userId);
+
     void removeLikeFilm(int filmId, int userId);
 }
